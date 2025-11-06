de8c5718b8603f83adc7859750d9fa2f8b2e1073
Shubhojit <shubhojit1655@gmail.com>
2025-11-06 22:37:16 +0530

    cors change

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";         
 import router from "./src/router.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: ["https://project-management-frontend-w48y.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api", router);
 
 app.get("/", (req, res) => {
-    res.send("RAG system running");
+  res.send("RAG system running");
 });
 
 app.listen(PORT, () => {
